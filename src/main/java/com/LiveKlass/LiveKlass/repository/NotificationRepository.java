a188c1da5e177e928bdb258fe18aa2297f209cdf
unknown <wkadht0619@gmail.com>
2026-04-05 22:16:59 +0900

    feat: 알림 목록 조회 API 필터링 로직 구현 #11

--- src/main/java/com/LiveKlass/LiveKlass/repository/NotificationRepository.java
+++ src/main/java/com/LiveKlass/LiveKlass/repository/NotificationRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
     List<Notification> findAllByUserId(Long userId);
+    List<Notification> findAllByUserIdAndIsRead(Long userId, boolean isRead);
 }
