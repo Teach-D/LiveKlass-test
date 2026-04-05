9d0ea1355a0b004bef329fe5bb935b6b03fa5734
unknown <wkadht0619@gmail.com>
2026-04-05 15:57:27 +0900

    first commit

---
+++ src/main/java/com/LiveKlass/LiveKlass/LiveKlassApplication.java
@@ -0,0 +1,13 @@
+package com.LiveKlass.LiveKlass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveKlassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveKlassApplication.class, args);
+	}
+
+}
